80e3f46d10d87d348a7795cc443819d1fff57020
adetrux <adetrux@gmail.com>
2020-02-24 10:51:43 +0100

    Add ocelot

--- src/ApiGateways/Ocelot.ApiGateway/Program.cs
+++ src/ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,9 +19,31 @@ namespace Ocelot.ApiGateway
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", false, true)
+                    //.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddJsonFile("ocelot.json", false, true);
+                    //.AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName}.json")
+                    //.AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        //public static IWebHost BuildWebHost(string[] args)
+        //{
+        //    var builder = WebHost.CreateDefaultBuilder(args);
+
+        //    builder.ConfigureServices(s => s.AddSingleton(builder))
+        //            .ConfigureAppConfiguration(
+        //                  ic => ic.AddJsonFile("ocelot.json"))
+        //            .UseStartup<Startup>();
+        //    var host = builder.Build();
+        //    return host;
+        //}
     }
 }
--- src/ApiGateways/Ocelot.ApiGateway/Startup.cs
+++ src/ApiGateways/Ocelot.ApiGateway/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 
 namespace Ocelot.ApiGateway
 {
