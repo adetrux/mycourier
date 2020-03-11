14e79fd1facb0532bb4165e577be9b841af3a5b4
adetrux <adetrux@gmail.com>
2020-03-11 11:22:45 +0100

    Add service ip alternatives

--- src/Clients/Mobile/mycouriermobile/shared/service/service.ts
+++ src/Clients/Mobile/mycouriermobile/shared/service/service.ts
@@ -1,5 +1,19 @@
+// export const service = {
+//   serviceBaseUrl: "http://192.168.0.80:5080",
+//   usersServiceApiUrl: "http://192.168.0.80:5080/users",
+//   deliverablesServiceApiUrl: "http://192.168.0.80:5080/deliverables"
+// };
+
+// -- Q
+// export const service = {
+//   serviceBaseUrl: "http://152.66.169.224:5080",
+//   usersServiceApiUrl: "http://152.66.169.224:5080/users",
+//   deliverablesServiceApiUrl: "http://152.66.169.224:5080/deliverables"
+// };
+
+// -- I
 export const service = {
-  serviceBaseUrl: "http://192.168.0.80:5080",
-  usersServiceApiUrl: "http://192.168.0.80:5080/users",
-  deliverablesServiceApiUrl: "http://192.168.0.80:5080/deliverables"
+  serviceBaseUrl: "http://152.66.158.220:5080",
+  usersServiceApiUrl: "http://152.66.158.220:5080/users",
+  deliverablesServiceApiUrl: "http://152.66.158.220:5080/deliverables"
 };
